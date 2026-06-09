ed05b6a60a4ead5237ef7a6c509c8d5aeaad9180
Thai Nguyen <thai.vc.nguyen@gmail.com>
2026-06-09 12:12:45 -0400

    do some OOP  exercises in Python

--- oopTest.py
+++ oopTest.py
@@ -1,17 +1,20 @@
-# The Blueprint
-class Dog:
-    # The Constructor (initializes attributes)
-    def __init__(self, name, breed):
-        self.name = name   # Attribute
-        self.breed = breed # Attribute
+#Create a class, create objects in OOP
+print("======EXERCISE 1======")
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = int(pages)
 
-    # A Method (behavior)
-    def bark(self):
-        return f"{self.name} says Woof!"
+    def display(self):
+        return f"Title: {self.title} by {self.author}, Page: {self.pages}"
+    
+book1 = Book("Harry Portter","J. K. Rowling",67)
+book2 = Book("Shoe Dog: A Memoir by the Creator of Nike","Phil Knight", 40 )
 
-# Creating Objects (Instances)
-dog1 = Dog("Buddy", "Golden Retriever")
-dog2 = Dog("Rex", "German Shepherd")
+print(book1.display())
+print(book2.display())
+print("======EXERCISE 1 END======")
+print("======EXERCISE 2======")
 
-print(dog1.bark()) # Output: Buddy says Woof!
-print(dog2.bark()) # Output: Buddy says Woof!
+
